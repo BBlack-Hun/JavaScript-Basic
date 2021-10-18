bbc039ed45e1ac90f62590f08ea3ca37bea6400a
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-10-18 20:47:13 +0900

    산술, 증감, 대입 연산자_21_10_18

---
+++ example2.js
@@ -0,0 +1,26 @@
+/**
+ * 연산자
+ */
+
+let value = 1; // =은 대입 연산자
+
+// 산술 연산자 => 사칙연산자 (+, -, *, /)
+
+let a = 1 + 2 - (3 * 4) / 2;
+console.log(a);
+
+// 증가 연산자
+
+let b = 1;
+
+console.log(b++);
+console.log(b);
+console.log(++b);
+
+// 대입 연산자
+
+let c = 1;
+c += 1; // 덧셈 말고, 뺄셈, 곱셈, 나눗셈 다 사용 가능하다.
+console.log(c);
+
+// 논리 연산자

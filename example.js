fd1ca36273068d8af9a42cca95120c61427d19ef
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-10-18 20:42:24 +0900

    변수 관련 선언 및 정의

--- example.js
+++ example.js
@@ -1,5 +1,32 @@
+// let으로 변수를 선언했을때 같은 변수명으로 선언할 수 없다. 단 다른 블록 내에서는 선언할 수 있다.
+
 let value;
 
-console.log('Hello JavaScript');
+let value = 0;
 
-console.log(test);
+// 상수를 선언 (const를 사용하여 선언)
+
+const a = 1;
+
+// var를 이용하여 변수를 선언하는 것은 추천하지 않는다.
+
+var b = 1;
+var b = 2;
+
+// 데이터 타입 (정수를 선언)
+let v = 1;
+
+// 문자열 선언
+let text = 'Hello';
+let name = '헬로우 자바스크립트';
+
+// boolean 선언
+
+let good = true;
+let loading = false;
+
+// null, undefined;
+
+let some = null; // 진짜 없다.
+
+let some2 = undefined; // 정해지지 않았다.

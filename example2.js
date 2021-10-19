8437c2cce8482c11b788996edd911d40b09c26a1
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-10-19 20:34:21 +0900

    논리 연산자_21_10_19

--- example2.js
+++ example2.js
@@ -24,3 +24,25 @@ c += 1; // 덧셈 말고, 뺄셈, 곱셈, 나눗셈 다 사용 가능하다.
 console.log(c);
 
 // 논리 연산자
+
+/**
+ * 연산 우선순위 대로 표기
+ * NOT !
+ * AND &&
+ * NOT !
+ */
+
+const d = !false;
+console.log(d);
+
+const e = true && true; // 양쪽이 모두 true일때만 true이다.
+
+const f = true || false; // 둘중의 하나만 true여도 ture이다.
+
+const value2 = !((true && false) || (true && false) || !false);
+// !(ture && false || ture && false || true)
+// !(false || false || true)
+// !(ture)
+// false
+
+console.log(value2);

635e3b420077b4d0df1aa922ebf048623b3101d9
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-10-25 22:31:20 +0900

    함수1_2021_10_25

---
+++ example5.js
@@ -0,0 +1,16 @@
+/**
+ * fucntion1
+ */
+
+function add(a, b) {
+  return a + b;
+}
+
+const sum = add(1, 2);
+console.log(sum);
+
+function Hello(name) {
+  console.log('Hello, ' + name + '!');
+}
+
+Hello('bblack_hun');

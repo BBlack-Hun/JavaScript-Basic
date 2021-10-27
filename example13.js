8df5493262c4843dc7ffef4272d766d8156ccc3d
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-10-27 21:44:15 +0900

    반복문1_2021_10_26

---
+++ example13.js
@@ -0,0 +1,17 @@
+/**
+ * 반복문
+ */
+
+// i를 선언, 0으로 초기화, i를 10전까지 0부터 1씩 증가시킨다.
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log('===============================');
+
+// 반복문 응용 -> 배열
+const names = ['멍멍이', '야옹이', '멍뭉이'];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
